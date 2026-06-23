d93cbc4f7729c2aa9ea5d752ef7f22c60d6f9efa
toruk-ai <80568263+toruk-ai@users.noreply.github.com>
2026-06-23 17:42:55 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  // 静态资源直接输出到根目录，public下txt可直接域名访问
-  publicDir: 'public'
-})
